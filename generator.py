ce46518b571446f0231326729235b488f7bb7dc8
OzoneGameDev <143538292+OzoneGameDev@users.noreply.github.com>
2024-01-28 20:00:14 +0100

    Add the program

---
+++ generator.py
@@ -0,0 +1,42 @@
+import secrets
+import string
+import tkinter as tk
+from tkinter import ttk
+
+alphabet = string.ascii_letters + string.digits + string.punctuation
+
+
+def generate():
+    length = int(lengthInput.get())
+    if length < 4:
+        length = 4
+    elif length > 100:
+        length = 100
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    print(password)
+
+
+
+window = tk.Tk()
+window.title("Password Generator")
+window.config(width=600)
+window.resizable(width=False, height=False)
+
+defaultValue = tk.IntVar(window, value=8)
+
+title = ttk.Label(window, text="Python Password Generator", font=("",25))
+title.pack(pady=10,padx=20)
+
+options = ttk.Frame(window)
+options.pack(pady=10)
+
+lengthText = ttk.Label(options, text="Length: ")
+lengthText.pack(side="left")
+
+lengthInput = ttk.Spinbox(options, from_=4, to=100, textvariable=defaultValue)
+lengthInput.pack()
+
+generateButton = ttk.Button(window, text="Generate!", command=generate)
+generateButton.pack(pady=10)
+
+window.mainloop()
